97f68b98a32920a0330dc89fe543b484ff4c0616
wangchun <2838138608@qq.com>
2020-08-10 10:13:00 +0800

    体检代码

--- ureport2-core/upload.bat
+++ ureport2-core/upload.bat
@@ -1,11 +1,11 @@
 set mvn_repo_home=G:\maven\repository\
 set deploy_tmp_folder=G:\maven\mvn_deploy\
 set repositoryId=maven-releases
-set repoUrl=http://192.168.31.188:8081/repository/maven-releases/
+set repoUrl=http://47.114.154.180:8081/repository/maven-releases/
 call mvn clean install
 
 
 xcopy %mvn_repo_home%com\bstek\ureport\ureport2-core\2.2.9\ureport2-core-2.2.9* %deploy_tmp_folder% /y
 call mvn deploy:deploy-file -DgroupId=com.bstek.ureport -DartifactId=ureport2-core -Dversion=2.2.9 -Dpackaging=jar -Dfile=%deploy_tmp_folder%ureport2-core-2.2.9.jar -Durl=%repoUrl% -DrepositoryId=%repositoryId%
-call mvn deploy:deploy-file -DgroupId=com.bstek.ureport -DartifactId=ureport2-core-sources -Dversion=2.2.9 -Dpackaging=jar -Dclassifier=sources -Dfile=%deploy_tmp_folder%ureport2-core-2.2.9-sources.jar -Durl=%repoUrl% -DrepositoryId=%repositoryId%
+call mvn deploy:deploy-file -DgroupId=com.bstek.ureport -DartifactId=ureport2-core -Dversion=2.2.9 -Dpackaging=jar -Dclassifier=sources -Dfile=%deploy_tmp_folder%ureport2-core-2.2.9-sources.jar -Durl=%repoUrl% -DrepositoryId=%repositoryId%
 call mvn deploy:deploy-file -DgroupId=com.bstek.ureport -DartifactId=ureport2-core -Dversion=2.2.9 -Dpackaging=pom -Dfile=%deploy_tmp_folder%ureport2-core-2.2.9.pom -Durl=%repoUrl% -DrepositoryId=%repositoryId%
--- ureport2-parent/upload.bat
+++ ureport2-parent/upload.bat
@@ -1,7 +1,7 @@
 set mvn_repo_home=G:\maven\repository\
 set deploy_tmp_folder=G:\maven\mvn_deploy\
 set repositoryId=maven-releases
-set repoUrl=http://192.168.31.188:8081/repository/maven-releases/
+set repoUrl=http://47.114.154.180:8081/repository/maven-releases/
 call mvn clean install
 
 xcopy %mvn_repo_home%com\bstek\ureport\ureport2-parent\2.0.6\ureport2-parent-2.0.6* %deploy_tmp_folder% /y
